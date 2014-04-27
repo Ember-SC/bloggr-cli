eb69ceeec71d29406f16e51db0ae48302d3fe1aa
Scott Smith <scottnelsonsmith@gmail.com>
2014-04-27 12:03:21 -0700

    Have posts data but not rendering yet

--- app/router.js
+++ app/router.js
@@ -1,8 +1,11 @@
 var Router = Ember.Router.extend({
+  rootURL: ENV.rootURL,
   location: 'auto'
 });
 
 Router.map(function() {
+    this.resource('posts');
+    this.resource('about');
 });
 
 export default Router;
