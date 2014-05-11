8d1c577eb019271073202980274cefc54cbfd1d5
Scott Smith <scottnelsonsmith@gmail.com>
2014-05-10 20:45:49 -0700

    Merged from Kyle's fix for cancel

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,9 +1,13 @@
 export default Ember.ObjectController.extend({
-    content: null,
     isEditing: false,
 
+
     edit: function() {
-        this.set('isEditing', true);
+	       this.set('oldTitle' , this.get('title'));
+         this.set('oldAuthor', this.get('author'));
+         this.set('oldExcerpt', this.get('excerpt'));
+         this.set('oldBody', this.get('body')) ;
+         this.set('isEditing', true);
     },
 
     doneEditing: function() {
@@ -12,20 +16,15 @@ export default Ember.ObjectController.extend({
     },
 
     cancelEditing: function() {
-        this.set('isEditing', false);
-        var restored = this.get('content');
-        restored.set('title', 'restored');
-        restored.set('author', { name: 'restored auth'});
-        this.set('content', restored);
+	     //this.set('content', this.get('oldContent'));
+       this.set('title' , this.get('oldTitle'));
+       this.set('author', this.get('oldAuthor'));
+       this.set('excerpt', this.get('oldExcerpt'));
+       this.set('body', this.get('oldBody'));
+	     this.set('isEditing', false);
+  	//this.set('content', this.get('oldContent'));
+
     }
+
+
 });
-
-/*
- id: '1',
- title: "Rails is Omakase",
- author: { name: "d2h" },
- date: new Date('12-27-2012'),
- excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
- body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
-
- */
