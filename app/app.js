7ff6eed96088cb2bd4bcaea452ab1d8f4431f386
Scott Smith <scottnelsonsmith@gmail.com>
2014-05-11 17:04:52 -0700

    Turned MODEL_FACTORY_INJECTIONS to false; App now is getting created rather than extended, and moved assignment of fixtures to the application adapter

--- app/app.js
+++ app/app.js
@@ -1,16 +1,14 @@
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
+Ember.MODEL_FACTORY_INJECTIONS = false;
 
-var App = Ember.Application.extend({
+var App = Ember.Application.create({
   modulePrefix: 'bloggr-cli', // TODO: loaded via config
   Resolver: Resolver
 });
+App.ApplicationAdapter = DS.FixtureAdapter;
 
 loadInitializers(App, 'bloggr-cli');
-
-//var App = Ember.Application.create();
-App.ApplicationAdapter = DS.FixtureAdapter;
 
 export default App;
